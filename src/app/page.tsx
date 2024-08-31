3e43e4e880f527e5051b4f70cbc716ccc103ec44
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2024-09-01 01:26:49 +1000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     <HydrateClient>
       <BaseCard className="w-40">
         <div className="font-bold">DEV Community is a community of 1,977,735 amazing developers</div>
-        <div className="text-black/50 text-[12px] py-3">We're a place where coders share, stay up-to-date and grow their careers.</div>
+        <div className="text-black/50 text-[12px] py-3">We&apos;re a place where coders share, stay up-to-date and grow their careers.</div>
         <div className="flex flex-col">
           <PrimaryBtn className="mb-1">Create account</PrimaryBtn>
           <SecondaryBtn>Log in</SecondaryBtn>
