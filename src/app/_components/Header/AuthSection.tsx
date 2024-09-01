6067b3f0a1f84ebb3d6122d71d676420b7ee1d93
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2024-09-02 02:10:06 +1000

    edit p img

--- src/app/_components/Header/AuthSection.tsx
+++ src/app/_components/Header/AuthSection.tsx
@@ -19,21 +19,21 @@ const AuthSection = () => {
     setIsOpen(!isOpen);
   };
 
-  // if (status === "unauthenticated") {
-  //   return (
-  //     <div className="flex flex-row-reverse w-1/2 h-full content-center items-center">
-  //       <PrimaryBtn onclick={() => router.push(pageRoutes.SIGNUP)}>Create account</PrimaryBtn>
-  //       <SecondaryBtn onclick={() => router.push(pageRoutes.LOGIN)} className="mr-[5px]">Log in</SecondaryBtn>
-  //     </div>
-  //   );
-  // }
+  if (status === "unauthenticated") {
+    return (
+      <div className="flex flex-row-reverse w-1/2 h-full content-center items-center">
+        <PrimaryBtn onclick={() => router.push(pageRoutes.SIGNUP)}>Create account</PrimaryBtn>
+        <SecondaryBtn onclick={() => router.push(pageRoutes.LOGIN)} className="mr-[5px]">Log in</SecondaryBtn>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-row-reverse items-center flex-grow">
       <div className="flex items-center rounded-full top-10 px-1 pt-1">
         <div className="relative">
           <button type="button" className="h-10 w-10 hover:bg-[#3b49df]/15 rounded-full p-[4px]" onClick={togglePopup}>
-            <Image className="rounded-full" src={session?.user?.image ?? "devto_ic.svg"} alt="" width={40} height={40} />
+            <Image className="rounded-full" src={session?.user?.image ?? "/devto_ic.svg"} alt="" width={40} height={40} />
             {/*<Link href={pageRoutes.PROFILES}/>*/}
           </button>
           {isOpen ? <ProfileDropCard/> : ""}
