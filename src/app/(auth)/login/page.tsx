568d6edfa80244cd9da7c866ea2ca7192460d4f5
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2024-09-01 17:48:20 +1000

    update login ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,15 +5,15 @@ import DevToLogo from "~/app/_components/Icon/DevToLogo";
 
 const Page = () => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 p-10">
       <div className="col-start-2 flex justify-center">
         <DevToLogo width={60} height={48}/>
       </div>
 
       <div className="col-start-2 flex flex-col justify-center mt-4">
-        <h1 className="font-black text-xl text-center">Join the DEV Community</h1>
+        <h1 className="font-black font-bold text-[30px] text-center">Join the DEV Community</h1>
         {/* TODO: make the number of user real time */}
-        <p className="text-black/50 text-xs text-center ">DEV Community is a community of 1,982,894 amazing developers</p>
+        <p className="text-black/50 text-base text-center ">DEV Community is a community of 1,982,894 amazing developers</p>
       </div>
 
       <div className="col-start-2 mt-4 ">
--- src/app/_components/Form/OAuthLoginForm.tsx
+++ src/app/_components/Form/OAuthLoginForm.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import Image from "next/image";
+import {signIn} from "next-auth/react";
+import Link from "next/link";
 
 const OAuthLoginForm = () => {
   const providers = [
     {
       name: "google",
       icon: "/gg.svg",
-      action: ""
+      link: "/api/auth/signin/google",
     },
     {
       name: "github",
       icon: "/git.svg",
-      action: ""
+      link: "/api/auth/signin/github"
     }
   ]
 
   return (
     <form className="flex h-screen w-full flex-col">
       {providers.map((item, index) => (
-        <button
+        <Link
           key={index}
           className="flex min-h-6 w-full flex-row items-center rounded border p-3 text-center text-xs mb-4"
-          type="submit"
-          name="action"
-          value={item.name}
+          href={item.link}
         >
           <Image className="justify-start" src={item.icon} alt={item.name} width={24} height={24} />
-          <p className="flex-grow">Continue with {item.name}</p>
-        </button>
+          <p className="flex-grow text-base">Continue with {item.name}</p>
+        </Link>
       ))}
     </form>
   );
